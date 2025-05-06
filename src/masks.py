06905a8b7754c91e95035de6ad52a38357b3d911
sorelon <astapov_lehe@mail.ru>
2025-05-06 22:50:55 +0300

    src/masks.py

---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Принимает на вход номер карты в формате - 7000792289606361,
+    и возвращает ее маску 7000 79** **** 6361."""
+
+    return card_number[:4] + " " + card_number[4:6] + "**" + " **** " + card_number[11:16]
+
+
+def get_mask_account(bank_account_number: str) -> str:
+    """Принимает на вход номер счета в формате- 73654108430135874305 и
+    возвращает его маску **4305."""
+
+    return "**" + bank_account_number[-4:]
