bf68f9020b84ecf3232bb1381c893ef152cfb055
sorelon <astapov_lehe@mail.ru>
2025-05-28 22:50:33 +0300

    init

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,37 @@
+from datetime import datetime
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(type_and_number: str) -> str:
-    """ Функция, которая маскирует номер счета или карты"""
-    text_result = ""
-    digit_result = ""
-    digit_count = 0
-    for el in type_and_number:
-        if el.isalpha():
-            text_result += el
-        elif el.isdigit():
-            digit_result += el
-            digit_count += 1
-    if digit_count > 16:
-        return f"{text_result} {get_mask_account(digit_result)}"
+def mask_account_card(info: str) -> str:
+    """Функция принимает строку с типом карты/счета и номер и возвращает строку с замаскированным номером"""
+    letters = ""
+    digits = ""
+
+    for char in info:  # Разделение букв и цифр из исходной строки
+        if char.isdigit():
+            digits += char
+        elif char.isalpha() or char.isspace():
+            letters += char
+
+    letters = letters.rstrip()  # Убираем лишние пробелы в конце строки
+
+    if info.lower().startswith("счет"):  # Определяем, маскировать номер счета или карты
+        masked_number = get_mask_account(digits)
     else:
-        return f"{text_result} {get_mask_card_number(digit_result)}"
+        masked_number = get_mask_card_number(digits)
+
+    return f"{letters} {masked_number}"
 
 
-def get_date(date_of_operation: str) -> str:
-    """ Функция, которая возвращает строку с датой в формате ДД.ММ.ГГГГ"""
-    return date_of_operation[8:10] + "." + date_of_operation[5:7] + "." + date_of_operation[0:4]
+def get_date(date_string: str) -> str:
+    """
+    Преобразует строку с датой в формате "2024-03-11T02:26:18.671407"
+    в формат "ДД.ММ.ГГГГ".
+
+    :param date_string: Строка с датой
+    в формате "2024-03-11T02:26:18.671407".
+    :return: Строка с датой в формате "ДД.ММ.ГГГГ".
+    """
+    date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
+    return date_object.strftime("%d.%m.%Y")
